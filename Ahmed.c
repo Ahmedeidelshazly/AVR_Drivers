8067d16a8fd51ccc11290d122fa348dc3bc9900d
Ahmed Eid Mohamed <ayd58824@gmail.com>
2024-05-19 22:45:57 +0300

    Modify file Ahmed.c

--- Ahmed.c
+++ Ahmed.c
@@ -4,15 +4,7 @@
 
 int main (void)
 {
-	int x = 10;
-	int y = 10; 
 	
-	int mohamud =100; 
-	char ff = 66; 
-	int hassan = 700; 
-	int x ;
-	int z = 70 ; 
-	int ahmedeidmohamed=77; 
  
 	printf("I Love Gehad Elsayed ");
 	
